6621fd2576e29dbaeb960c4e53df17f76a61e9ee
Curtis <curtisadams28@gmail.com>
2022-12-08 12:46:28 +1300

    Added player select screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,23 @@
 import "./App.css";
+import PlayerSelect from './components/PlayerSelect';
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
 
 const socket = io.connect("http://192.168.1.109:3001");
 
+
+
 function App() {
-  //Room State
-  const [room, setRoom] = useState("");
 
-  /*
-  const joinRoom = () => {
-    setRoom(1)
-    socket.emit("join_room", room);
-  };
-
-  const sendMessage = () => {
-    socket.emit("send_message", { message, room });
-  };
-
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setMessageReceived(data.message);
-    });
-  }, [socket]);
-  */
-
-  const sendPercentage = (e) => {
-    socket.emit('send_percentage', e.target.value)
-  }
+  const [playerType, setPlayerType] = useState(null);
+  console.log(playerType);
 
   return (
-    <div className="App">
-      <h1>Chance</h1>
-      <div className="chance-buttons">
-        <button value='0%' onClick={sendPercentage}>0%</button>
-      </div>
-
+    <div className="wrapper">
+      <PlayerSelect playerType = {playerType} setPlayerType = {setPlayerType}/>
     </div>
   );
 }
 
 export default App;
+
---
+++ client/src/components/PlayerSelect.js
@@ -0,0 +1,19 @@
+function PlayerSelect(props) {
+  console.log(props);
+
+  function handleClick(e) {
+    props.setPlayerType(e.target.value);
+  }
+
+  if (props.playerType === null) {
+    return (
+      <div className="player-selector">
+        <h1>Select User</h1>
+        <button value='gm' onClick={handleClick}>Game Master</button>
+        <button value='player' onClick={handleClick}>Player</button>
+      </div>
+    );
+  }
+}
+
+export default PlayerSelect;
